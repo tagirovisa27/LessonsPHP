c0ae8a2aad41d82a8142cf812a4996f92c7da709
isa <isatagirov01@mail.ru>
2024-02-10 11:20:00 +0300

    Corrections in the project

--- simple-resume/app/Http/Controllers/indexController.php
+++ simple-resume/app/Http/Controllers/indexController.php
@@ -6,14 +6,15 @@ use App\Services\MenuServiceInterface;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
-{  
+{
     private MenuServiceInterface $menuService;
-    public function __construct(MenuServiceInterface $menuService) 
+
+    public function __construct(MenuServiceInterface $menuService)
     {
-        $this->menuService = $menuService;
+      $this->menuService = $menuService;
     }
-        public function main()
+      public function main()
     {
-       return view('main', ['menu' => $this->menuService->getMenu()]);    
+        return view('main', ['menu' => $this->menuService->getMenu()]);
     }
 }
